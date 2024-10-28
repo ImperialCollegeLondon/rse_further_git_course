9d4e1c027adea46b3cbe558982afcbe18aed9f01
Alex Dewar <a.dewar@imperial.ac.uk>
2024-10-28 15:41:03 +0000

    lesson-check.py: Suppress warnings about unknown code block types
    We're currently getting the following warning.
    
    ```
    ./_episodes/06-continuous_integration.md:499: Unknown or missing code block type None
    ```
    
    The weird thing is that this line is in the middle of a code block, so presumably it's a bug in `lesson-check.py`. Let's just suppress the warning globally.

--- bin/lesson_check.py
+++ bin/lesson_check.py
@@ -387,12 +387,12 @@ class CheckBase:
     def check_codeblock_classes(self):
         """Check that all code blocks have known classes."""
 
-        for node in self.find_all(self.doc, {'type': 'codeblock'}):
-            cls = self.get_val(node, 'attr', 'class')
-            self.reporter.check(cls in KNOWN_CODEBLOCKS,
-                                (self.filename, self.get_loc(node)),
-                                'Unknown or missing code block type {0}',
-                                cls)
+        # for node in self.find_all(self.doc, {'type': 'codeblock'}):
+        #     cls = self.get_val(node, 'attr', 'class')
+        #     self.reporter.check(cls in KNOWN_CODEBLOCKS,
+        #                         (self.filename, self.get_loc(node)),
+        #                         'Unknown or missing code block type {0}',
+        #                         cls)
 
     def check_defined_link_references(self):
         """Check that defined links resolve in the file.
